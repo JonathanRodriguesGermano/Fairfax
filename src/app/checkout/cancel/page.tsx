a9098628ce30caa107e1034e4fdf624e015773fa
jonathanrodriguesgermano <jonathanrodriguesgermano7@gmail.com>
2026-04-05 18:32:20 -0300

    feat: implement order confirmation functionality

---
+++ src/app/checkout/cancel/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CheckoutCancelPage = () => {
+  return (
+    <>
+      <div>CheckoutSuccessPage</div>
+    </>
+  );
+};
+
+export default CheckoutCancelPage;
--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import { Button } from "@/components/ui/button";
 import {
@@ -16,37 +17,44 @@ import {
 const CheckoutSuccessPage = () => {
   return (
     <>
-      <Header />
-      <Dialog open={true} onOpenChange={() => {}}>
-        <DialogContent className="text-center">
-          <Image
-            src="/illustration.svg"
-            alt="Success"
-            width={300}
-            height={300}
-            className="mx-auto"
-          />
-          <DialogTitle className="mt-4 text-2xl">Pedido efetuado!</DialogTitle>
-          <DialogDescription className="font-medium">
-            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
-            na seção de “Meus Pedidos”.
-          </DialogDescription>
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <div className="flex-1">
+          <Dialog open={true} onOpenChange={() => {}}>
+            <DialogContent className="text-center">
+              <Image
+                src="/illustration.svg"
+                alt="Success"
+                width={300}
+                height={300}
+                className="mx-auto"
+              />
+              <DialogTitle className="mt-4 text-2xl">
+                Pedido efetuado!
+              </DialogTitle>
+              <DialogDescription className="font-medium">
+                Seu pedido foi efetuado com sucesso. Você pode acompanhar o
+                status na seção de “Meus Pedidos”.
+              </DialogDescription>
 
-          <DialogFooter>
-            <Button className="rounded-full" size="lg">
-              Ver meus pedidos
-            </Button>
-            <Button
-              className="rounded-full"
-              variant="outline"
-              size="lg"
-              asChild
-            >
-              <Link href="/">Voltar para a loja</Link>
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+              <DialogFooter>
+                <Button className="rounded-full" size="lg" asChild>
+                  <Link href="my-orders">Ver meus pedidos</Link>
+                </Button>
+                <Button
+                  className="rounded-full"
+                  variant="outline"
+                  size="lg"
+                  asChild
+                >
+                  <Link href="/">Voltar para a loja</Link>
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 };
